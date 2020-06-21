ca2a3aa43fcdf00a45679d10a2688200184c224a
Tomahawk <47765953+IAmTomahawkx@users.noreply.github.com>
2020-06-21 03:06:49 -0700

    Add files via upload

---
+++ ext/checks.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+class NoMod(commands.CheckFailure):
+    pass
+
+class NoSomething(commands.CommandError):
+    def __init__(self, account: discord.User):
+        self.user = account
+        if account.bot:
+            fmt = f"The bot {account} wasn't found in my database"
+        else:
+            fmt = f"The user {account} wasn't found in my database"
+
+        self.message = fmt
+        super().__init__(fmt)
+
+def mod_check():
+    async def predicate(ctx):
+        db_user = await ctx.bot.db.users.find_one({"_id": str(ctx.author.id)})
+        if db_user is None or not db_user['rank']['mod']:
+            raise NoMod()
+
+        return True
+
+    return commands.check(predicate)
