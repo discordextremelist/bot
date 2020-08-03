aa559678631a1a6cf98940db793b360d9814d6d8
DuckMasterAl <43557963+DuckMasterAl@users.noreply.github.com>
2020-08-03 17:10:47 -0400

    This needs to be an int as well

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -66,7 +66,7 @@ class TicketCog(commands.Cog):
 
             owner = ctx.guild.get_member(int(bot_db["owner"]["id"]))
             mods = ctx.guild.get_role(int(self.bot.settings["roles"]["mod"]))
-            serverbots = ctx.guild.get_role(self.bot.settings['roles']['botpower']) # bot power role
+            serverbots = ctx.guild.get_role(int(self.bot.settings['roles']['botpower']))
 
             overwrites = {
                 ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
