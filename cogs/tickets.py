f2b55be14715b37d66d7497c9f3488062680d002
Duck Master <43557963+DuckMasterAl@users.noreply.github.com>
2020-08-03 16:13:52 -0400

    Added a check in case the bot isn't in the server
    The bot may not be in the server since it's spamming something or because we're testing the `open` command.

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -108,7 +108,8 @@ class TicketCog(commands.Cog):
             delguild = self.bot.get_guild(632908146305925129)
             fixesrole = delguild.get_role(662230379326865429)
             memberbot = delguild.get_member(bot.id)
-            await memberbot.add_roles(fixesrole, reason='Bot is Awaiting Fixes.')
+            if memberbot is not None:
+                await memberbot.add_roles(fixesrole, reason='Bot is Awaiting Fixes.')
 
             log_channel = ctx.guild.get_channel(int(self.bot.settings["channels"]["ticketLog"]))
             log_msg = await log_channel.send(embed=embed2)
