b60195cf5f78284f2c77b563a47eedd2c560c275
Moksej <58531286+TheMoksej@users.noreply.github.com>
2020-10-17 23:40:15 +0200

    update #1
    This adds stuff that I'll need later on. I'm not going to make a pr with this because:
    1. It's not finished.
    2. I need to do proper testing before adding code to here

--- cogs/automod.py
+++ cogs/automod.py
@@ -2,9 +2,14 @@ import discord
 
 from discord.ext import commands
 
+class CooldownByContent(commands.CooldownMapping):
+    def _bucket_key(ctx, message):
+        return (message.channel.id, message.content)
+      
 class Automod(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
+    self.messages_cooldown = CooldownByContent.from_cooldown(15, 17.0, commands.BucketType.member)
     
 def setup(bot):
   bot.add_cog(Automod(bot))
