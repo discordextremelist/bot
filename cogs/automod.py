547bcc659652a8bf59c476014136adeea80fafa0
Moksej <58531286+TheMoksej@users.noreply.github.com>
2020-10-25 18:15:26 +0100

    adding whitelisted guilds

--- cogs/automod.py
+++ cogs/automod.py
@@ -4,7 +4,8 @@ import re
 from discord.ext import commands
 from datetime import datetime, timezone
 
-INVITE = re.compile(r"(?:https?://)?discord(?:\.com/invite|app\.com/invite|\.gg)/?[a-zA-Z0-9]+/?")
+# this regex is probably trash but at least it works
+INVITE = re.compile(r"(?:https?://)?discord(?:\.com/invite|app\.com/invite|\.gg)/(?:discord(?:\-api|\-developers|\-testers)|[a-zA-Z0-9])+/?")
 
 
 class CooldownByContent(commands.CooldownMapping):
@@ -15,9 +16,11 @@ class CooldownByContent(commands.CooldownMapping):
 class Automod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        # cooldown goes the same as commands.cooldown
+        # (messages, seconds, user/member/channel whatever)
         self.messages_cooldown = CooldownByContent.from_cooldown(15, 17.0, commands.BucketType.member)  # checks for same content
         self.user_cooldowns = commands.CooldownMapping.from_cooldown(10, 12.0, commands.BucketType.user)  # checks for member spam
-        self.invite_cooldown = commands.CooldownMapping.from_cooldown(5, 10, commands.BucketType.user)  # checks for invites
+        self.invite_cooldown = commands.CooldownMapping.from_cooldown(5, 60.0, commands.BucketType.user)  # checks for invites
 
     # if they send a message
     @commands.Cog.listener('on_message')
@@ -27,7 +30,7 @@ class Automod(commands.Cog):
         # if automod == 0:
         #     return
 
-        if message.guild is None or message.guild.id != 667065302260908032:  # in DM's or not DEL server
+        if message.guild is None or message.guild.id != 568567800910839811:  # in DM's or not DEL server
             return
 
         if message.author not in message.guild.members:  # They were banned but messages are still being sent cause discord
@@ -76,7 +79,7 @@ class Automod(commands.Cog):
         content_bucket = self.messages_cooldown.get_bucket(message)
         if content_bucket.update_rate_limit(current):
             try:
-                # ! Check if user is already muted
+                # Check if user is already muted
                 muterole = discord.utils.find(lambda r: r.name.lower() == "muted", message.guild.roles)
                 if muterole not in message.author.roles:
                     await message.author.add_roles(muterole, reason=reason)
@@ -90,7 +93,7 @@ class Automod(commands.Cog):
         user_bucket = self.user_cooldowns.get_bucket(message)
         if user_bucket.update_rate_limit(current):
             try:
-                # ! Check if user is already muted
+                # Check if user is already muted
                 muterole = discord.utils.find(lambda r: r.name.lower() == "muted", message.guild.roles)
                 if muterole not in message.author.roles:
                     await message.author.add_roles(muterole, reason=reason)
@@ -106,17 +109,45 @@ class Automod(commands.Cog):
         reason = 'potential advertising'
         logchannel = message.guild.get_channel()  # put channel id here
 
+        # this include all the library support server
+        # discord-api, discord-testers and discord-developers
+        # if you want to add more, just copy that server id
+        # and paste it in this list. That'll make it whitelisted
+        # to save up resources you can paste DEL's server id
+        # in here as well, and remove lines 142-143
+        whitelisted_guilds = [197038439483310086, 336642139381301249, 81384788765712384, 613425648685547541, 493130730549805057,
+                              222078108977594368, 125227483518861312, 208023865127862272, 151037561152733184, 379378609942560770,
+                              530557949098065930, 486311607400529931, 223909216866402304]
+
         if invites:
             current = message.created_at.replace(tzinfo=timezone.utc).timestamp()
             content_bucket = self.invite_cooldown.get_bucket(message)
             guild_invs = []
+            # this will work as long as they won't provide
+            # all the whitelisted invites. However if they will
+            # it'll take ~10-30 seconds for loop to find non-whitelisted invite
+            # If they'll spam messages too fast anti spam will mute them.
             for inv in invites:
+                try:
+                    check_whitelist = await self.bot.fetch_invite(inv)
+                    if check_whitelist.guild.id in whitelisted_guilds:
+                        # if the original message is "discord.gg/invite" and not "https://discord.gg/invite"
+                        # this whitelisting will not work on "https://discord.com/invite/randominv"
+                        # as `check_whitelist.url` will provide "https://discord.gg/randominv" url
+                        if "https://" not in inv:
+                            guild_invs.append(check_whitelist.url[8:])
+                        else:
+                            guild_invs.append(check_whitelist.url)
+                except Exception as e:
+                    print(e)
+                    pass
                 for invs in await message.guild.invites():
                     guild_invs.append(invs.url)
+
                 if inv not in guild_invs:
                     await message.delete()
                     if content_bucket.update_rate_limit(current):
-                        # ! Check if user is already muted
+                        # Check if user is already muted
                         muterole = discord.utils.find(lambda r: r.name.lower() == "muted", message.guild.roles)
                         if muterole not in message.author.roles:
                             await message.author.add_roles(muterole, reason=reason)
@@ -138,7 +169,7 @@ class Automod(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     @commands.bot_has_permissions(manage_messages=True)
     async def toggleautomod(self, ctx):
-        check = # get value from cache
+        check = None  # get value from cache
 
         if check == 0:
             # update cache
