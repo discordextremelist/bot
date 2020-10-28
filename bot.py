0b4df9104bf8a81aaeb9955d64a9e5a6817839f3
Cairo <cairoacason.john@gmail.com>
2020-10-28 16:13:08 +1300

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,8 +56,7 @@ async def get_prefix(bot, message):
         prefixes = settings["prefix"]
         return commands.when_mentioned_or(*prefixes)(bot, message)
 
-intents = discord.Intents(guilds=True, members=True, bans=False, emojis=False, integrations=False, webhooks=False, invites=False, voice_states=False,
-                          presences=False, messages=True, reactions=True, typing=False)
+intents = discord.Intents(guilds=True, members=True, messages=True, reactions=True)
     
 if settings["ownership"]["multiple"]:
     bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, owner_ids=settings["ownership"]["owners"],
